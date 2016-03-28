0061a6e0f70051a11a85f67bd189f754d43db8d4
Ali Alsaihaty <AliAlsaihaty@Alis-MacBook-Pro.local>
2016-03-28 16:37:56 -0700

    in-person page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+  def in_person
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-
+  get 'in_person/' => 'home#in_person'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
